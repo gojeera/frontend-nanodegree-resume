6809d103e070244f296f3d807a63a34c2245f236
Ysmael Castillo <ysmael.castillo@gmail.com>
2016-06-06 20:28:03 -0400

    bio JSON
    created bio JSON object

---
+++ js/resumeBuilder.js
@@ -0,0 +1,16 @@
+//bio JSON
+var bio = {
+	"name" : "Ysmael Kurt Castillo",
+	"role" : "Front-End Web Dev",
+	"contacts" : {
+		"mobile" : "(555) 555-5555",
+		"email" : "ysmael.castillo@gmail.com",
+		"github" : "https://github.com/gojeera",
+		"location" : "Toronto, ON"
+	},
+	"welcomeMessage" : "Yo.",
+	"skills" : ["stuff", "more stuff"],
+	"biopic" : "http://placehold.it/150x150"
+};
+
+	
