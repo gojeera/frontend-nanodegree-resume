9e0698ae13fe77e9e63b1c544677061864e560b9
Ysmael Castillo <ysmael.castillo@gmail.com>
2016-06-06 20:42:01 -0400

    work JSON
    added work JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,8 +9,22 @@ var bio = {
 		"location" : "Toronto, ON"
 	},
 	"welcomeMessage" : "Yo.",
-	"skills" : ["stuff", "more stuff"],
+	"skills" : ["stuff", "more stuff", "even more stuff"],
 	"biopic" : "http://placehold.it/150x150"
 };
 
-	
+//work JSON
+var work = {
+	"jobs" : [{
+		"employer" : "Current Employer",
+		"title" : "not the boss",
+		"dates" : "2010 - Current",
+		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	},
+	{
+		"employer" : "Previous Employer",
+		"title" : "not the boss",
+		"dates" : "2005 - 2010",
+		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	}]
+};
