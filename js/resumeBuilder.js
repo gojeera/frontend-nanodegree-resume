4a6716f4cf1ab336d86e875514c42404c2384aad
Ysmael Castillo <ysmael.castillo@gmail.com>
2016-06-06 21:08:46 -0400

    education JSON
    completed education JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,8 +20,7 @@ var work = {
 		"title" : "not the boss",
 		"dates" : "2010 - Current",
 		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	},
-	{
+	}, {
 		"employer" : "Previous Employer",
 		"title" : "not the boss",
 		"dates" : "2005 - 2010",
@@ -35,8 +34,7 @@ var projects = [{
 	"dates" : "2016",
 	"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	"images" : "http://placehold.it/150x150"
-},
-{
+}, {
 	"title" : "Project 2",
 	"dates" : "2016",
 	"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -45,5 +43,32 @@ var projects = [{
 
 //education JSON
 var education = {
-	"schools" = [{
+	"schools" : [{
+		"name" : "York University",
+		"location" : "North York, ON",
+		"degree" : "Bachelor of Arts",
+		"majors" : "Business Administration",
+		"dates" : "2001 - 2005", 
+		"url" : "http://www.yorku.ca/index.html"
+	}, {
+		"name" : "Made Up School",
+		"location" : "Atlanitc Ocean",
+		"degree" : "Bachelor of Fish",
+		"majors" : "Sea Creatures",
+		"dates" : "2001 - 2005", 
+		"url" : "https://www.ripleyaquariums.com/canada/"
+	}],
+	"onlineCourses" : [{
+		"title" : "Front-End Web Developer",
+		"school" : "Udacity",
+		"dates" : "2016",
+		"url": "https://www.udacity.com/"
+	}, {
+		"title" : "Video Game Nerd",
+		"school" : "SIFTD",
+		"dates" : "2016",
+		"url": "http://siftd.net/"
+	}]
+};
+	
 		
