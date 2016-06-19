2f5c5eb28e7fac6ce30ba58f2585229fb911e9de
Ysmael Castillo <ysmael.castillo@gmail.com>
2016-06-19 15:40:35 -0400

    completed
    finsihed customization and beautified code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,196 +1,194 @@
 //bio
 var bio = {
-	"name" : "Ysmael Kurt Castillo",
-	"role" : "Front-End Web Dev",
-	"contacts" : {
-		"mobile" : "(555) 555-5555",
-		"email" : "ysmael.castillo@gmail.com",
-		"github" : "https://github.com/gojeera",
-		"location" : "Toronto, ON"
-	},
-	"welcomeMessage" : "Yo.",
-	"skills" : ["stuff", "more stuff", "even more stuff"],
-	"biopic" : "http://placehold.it/150x150"
+    "name": "Ysmael Kurt Castillo",
+    "role": "Front-End Web Dev",
+    "contacts": {
+        "mobile": "(555) 555-5555",
+        "email": "ysmael.castillo@gmail.com",
+        "github": "https://github.com/gojeera",
+        "location": "Toronto, ON"
+    },
+    "welcomeMessage": "Yo.",
+    "skills": ["stuff", "more stuff", "even more stuff"],
+    "biopic": "http://placehold.it/150x150"
 };
 
 function displayBio() {
-	var formattedName = HTMLheaderName.replace("%data%", bio.name);
-	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-	var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
-	var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+    var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
 
-	$("#header").prepend(formattedRole);
-	$("#header").prepend(formattedName);
-	$("#header").append(formattedWelcomeMsg);
-	$("#header").append(formattedBioPic);
+    $("#header").prepend(formattedRole);
+    $("#header").prepend(formattedName);
+    $("#header").append(formattedWelcomeMsg);
+    $("#header").append(formattedBioPic);
 
-	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
-	$("#topContacts").append(formattedMobile);
-	$("#topContacts").append(formattedEmail);
-	$("#topContacts").append(formattedGithub);
-	$("#topContacts").append(formattedLocation);
+    $("#topContacts").append(formattedMobile);
+    $("#topContacts").append(formattedEmail);
+    $("#topContacts").append(formattedGithub);
+    $("#topContacts").append(formattedLocation);
 
-	$("#footerContacts").append(formattedMobile);
-	$("#footerContacts").append(formattedEmail);
-	$("#footerContacts").append(formattedGithub);
-	$("#footerContacts").append(formattedLocation);
+    $("#footerContacts").append(formattedMobile);
+    $("#footerContacts").append(formattedEmail);
+    $("#footerContacts").append(formattedGithub);
+    $("#footerContacts").append(formattedLocation);
 
-	$("#header").append(HTMLskillsStart);
-	for (i = 0; i < bio.skills.length; i++) {
-		var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
+    $("#header").append(HTMLskillsStart);
+    for (i = 0; i < bio.skills.length; i++) {
+        var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
 
-		$("#skills").append(formattedSkills);
-	}
+        $("#skills").append(formattedSkills);
+    }
 }
 displayBio();
 
 //work
 var work = {
-	"jobs" : [{
-		"employer" : "Current Employer",
-		"title" : "not the boss",
-		"dates" : "2010 - Current",
-		"location" : "Toronto, ON",
-		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	}, {
-		"employer" : "Previous Employer",
-		"title" : "not the boss",
-		"dates" : "2005 - 2010",
-		"location" : "Mississauga, ON",
-		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	}]
+    "jobs": [{
+        "employer": "Current Employer",
+        "title": "not the boss",
+        "dates": "2010 - Current",
+        "location": "Toronto, ON",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }, {
+        "employer": "Previous Employer",
+        "title": "not the boss",
+        "dates": "2005 - 2010",
+        "location": "Mississauga, ON",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }]
 };
 
 function displayWork() {
-	for(job in work.jobs) {
-		$("#workExperience").append(HTMLworkStart);
+    for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
 
-		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-		var formattedEmployerTitle = formattedEmployer + formattedTitle;
-		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
 
-		$(".work-entry:last").append(formattedEmployerTitle);
-		$(".work-entry:last").append(formattedDates);
-		$(".work-entry:last").append(formattedLocation);
-		$(".work-entry:last").append(formattedDescription);
-	}
+        $(".work-entry:last").append(formattedEmployerTitle);
+        $(".work-entry:last").append(formattedDates);
+        $(".work-entry:last").append(formattedLocation);
+        $(".work-entry:last").append(formattedDescription);
+    }
 }
 
-displayWork ();
+displayWork();
 
 //projects
 var projects = {
-	"projects" : [{
-		"title" : "Project 1",
-		"dates" : "2016",
-		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		"images" : ["http://placehold.it/150x150", "http://placehold.it/150x150"]
-	}, {
-		"title" : "Project 2",
-		"dates" : "2016",
-		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		"images" : ["http://placehold.it/150x150", "http://placehold.it/150x150"]
-	}]
+    "projects": [{
+        "title": "Project 1",
+        "dates": "2016",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "images": ["http://placehold.it/150x150", "http://placehold.it/150x150"]
+    }, {
+        "title": "Project 2",
+        "dates": "2016",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "images": ["http://placehold.it/150x150", "http://placehold.it/150x150"]
+    }]
 };
 
 function displayProjects() {
-	for(project in projects.projects) {
-		$("#projects").append(HTMLprojectStart);
+    for (project in projects.projects) {
+        $("#projects").append(HTMLprojectStart);
 
-		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
 
-		$(".project-entry:last").append(formattedTitle);
-		$(".project-entry:last").append(formattedDates);
-		$(".project-entry:last").append(formattedDescription);
+        $(".project-entry:last").append(formattedTitle);
+        $(".project-entry:last").append(formattedDates);
+        $(".project-entry:last").append(formattedDescription);
 
-		if(projects.projects[project].images.length > 0) {
-			for(image in projects.projects[project].images) {
-				var formattedImages = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+        if (projects.projects[project].images.length > 0) {
+            for (image in projects.projects[project].images) {
+                var formattedImages = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
 
-				$(".project-entry:last").append(formattedImages);
-			}
-		}
-	}
+                $(".project-entry:last").append(formattedImages);
+            }
+        }
+    }
 
 }
 displayProjects();
 
 //education
 var education = {
-	"schools" : [{
-		"name" : "York University",
-		"degree" : "Bachelor of Arts",
-		"dates" : "2001 - 2005",
-		"majors" : "Business Administration",
-		"location" : "North York, ON",
-		"url" : "http://www.yorku.ca/index.html"
-	}, {
-		"name" : "Made Up School",
-		"degree" : "Bachelor of Fish",
-		"dates" : "2001 - 2005",
-		"majors" : "Sea Creatures",
-		"location" : "Atlanitc Ocean",
-		"url" : "https://www.ripleyaquariums.com/canada/"
-	}],
-	"onlineCourses" : [{
-		"title" : "Front-End Web Developer",
-		"school" : "Udacity",
-		"dates" : "2016",
-		"url": "https://www.udacity.com"
-	}, {
-		"title" : "Video Game Nerd",
-		"school" : "SIFTD",
-		"dates" : "2016",
-		"url": "http://siftd.net"
-	}]
+    "schools": [{
+        "name": "York University",
+        "degree": "Bachelor of Arts",
+        "dates": "2001 - 2005",
+        "majors": "Business Administration",
+        "location": "North York, ON",
+        "url": "http://www.yorku.ca/index.html"
+    }, {
+        "name": "Made Up School",
+        "degree": "Bachelor of Fish",
+        "dates": "2001 - 2005",
+        "majors": "Sea Creatures",
+        "location": "Atlanitc Ocean",
+        "url": "https://www.ripleyaquariums.com/canada/"
+    }],
+    "onlineCourses": [{
+        "title": "Front-End Web Developer",
+        "school": "Udacity",
+        "dates": "2016",
+        "url": "https://www.udacity.com"
+    }, {
+        "title": "Video Game Nerd",
+        "school": "SIFTD",
+        "dates": "2016",
+        "url": "http://siftd.net"
+    }]
 };
 
 function displayEducation() {
-	for (school in education.schools) {
-		$("#education").append(HTMLschoolStart);
+    for (school in education.schools) {
+        $("#education").append(HTMLschoolStart);
 
-		var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
-		var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-		var formattedNameDegree = formattedName + formattedDegree;
-		var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-		var formattedMajors = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
-		var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-		var formattedUrl = HTMLschoolURL.replace("%data%", education.schools[school].url);
+        var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        var formattedNameDegree = formattedName + formattedDegree;
+        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        var formattedMajors = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+        var formattedUrl = HTMLschoolURL.replace("%data%", education.schools[school].url);
 
-		$(".education-entry:last").append(formattedNameDegree);
-		$(".education-entry:last").append(formattedDates);
-		$(".education-entry:last").append(formattedMajors);
-		$(".education-entry:last").append(formattedLocation);
-		$(".education-entry:last").append(formattedUrl);
-	}
+        $(".education-entry:last").append(formattedNameDegree);
+        $(".education-entry:last").append(formattedDates);
+        $(".education-entry:last").append(formattedMajors);
+        $(".education-entry:last").append(formattedLocation);
+        $(".education-entry:last").append(formattedUrl);
+    }
 
-	$("#education").append(HTMLonlineStart);
-	$(".online-courses").append(HTMLonlineClasses);
+    $("#education").append(HTMLonlineStart);
+    $(".online-courses").append(HTMLonlineClasses);
 
-	for (online in education.onlineCourses) {
-		var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[online].title);
-		var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[online].school);
-		var formattedTitleSchool = formattedTitle + formattedSchool;
-		var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[online].dates);
-		var formattedUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[online].url);
+    for (online in education.onlineCourses) {
+        var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[online].title);
+        var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[online].school);
+        var formattedTitleSchool = formattedTitle + formattedSchool;
+        var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[online].dates);
+        var formattedUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[online].url);
 
-		$(".online-courses:last").append(formattedTitleSchool);
-		$(".online-courses:last").append(formattedDates);
-		$(".online-courses:last").append(formattedUrl);
-	}
+        $(".online-courses:last").append(formattedTitleSchool);
+        $(".online-courses:last").append(formattedDates);
+        $(".online-courses:last").append(formattedUrl);
+    }
 }
 displayEducation();
 
 //map
 $("#mapDiv").append(googleMap);
-
-
